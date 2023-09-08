46307ede2e46f82e338bc2284dad3634c111d0b7
Tahagod <tahagodibrahim@gmail.com>
2023-09-07 19:25:08 -0700

    C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
- * Description: A C program that prints a line to the standard error.
+ * Description: print a quote using write function 
+ * 	write(int fd, const void *buf, size_t count)
  *
- * Return: Always 1 (failure)
+ * Return: 1 (not success)
 */
+
 int main(void)
 {
-	printf"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, quo, 59);
 	return (1);
 }	
