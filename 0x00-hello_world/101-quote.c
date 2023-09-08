8845728b159e9203dd678d6fed747a58871c1f27
Tahagod <tahagodibrahim@gmail.com>
2023-09-07 19:11:49 -0700

    C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,10 @@
  *
  * Description: A C program that prints a line to the standard error.
  *
- * Return: Always 1 (Success)
+ * Return: Always 1 (failure)
 */
 int main(void)
 {
-	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59)
+	printf"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }	
