628f3d3d3f737d7f427b4e74d33482aef3645a1b
Tahagod <tahagodibrahim@gmail.com>
2023-11-12 14:04:01 -0800

    Awwww

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,15 +15,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	ssize_t byte;
 
-	char buf(READ_BUF_SIZE * 8);
+	char buf[READ_BUF_SIZE * 8];
 
 	if (!filename || !letters)
 		return (0);
 	ab = open(filename, O_RDONLY);
 	if (ab == -1)
 		return (0);
-	bytes = read(ab, &buf[0], letters);
-	bytes = write(STDOUT_FILENO, &buf[0], byte);
+	byte = read(ab, &buf[0], letters);
+	byte = write(STDOUT_FILENO, &buf[0], byte);
 	close(ab);
 	return (byte);
 }
